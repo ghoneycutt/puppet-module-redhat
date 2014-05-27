a73a7a3da76c173eed5cd89397d33162fa8944f9
Garrett Honeycutt <code@garretthoneycutt.com>
2014-05-27 11:03:00 -0400

    Add ability to specify mode of root's .bashrc

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -44,4 +44,33 @@ describe 'redhat' do
       })
     }
   end
+
+  describe 'with root_bashrc_mode specified' do
+    context 'as a valid mode' do
+      let(:params) { { :root_bashrc_mode => '0600' } }
+
+      it {
+        should contain_file('root_bashrc').with({
+          'ensure' => 'file',
+          'path'   => '/.bashrc',
+          'source' => 'puppet:///modules/redhat/root_bashrc',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0600',
+        })
+      }
+    end
+
+    [true,'666','66666'].each do |mode|
+      context "as invalid mode #{mode}" do
+        let(:params) { { :root_bashrc_mode => mode } }
+
+        it 'should fail' do
+          expect {
+            should contain_class('redhat')
+          }.to raise_error(Puppet::Error,/^redhat::root_bashrc_mode is <#{mode}> and must be a valid four digit mode in octal notation./)
+        end
+      end
+    end
+  end
 end
